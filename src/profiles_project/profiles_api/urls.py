1cdbf39c413f0a20eca6ff6edc1a63496a87b88d
prahladchaudhary14 <53113794+prahladchaudhary14@users.noreply.github.com>
2020-08-07 09:53:10 +0530

    api created

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from .import views
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
 
+router=DefaultRouter()
+"""router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')"""
+router.register('profile',views.UserProfileViewSet)
 urlpatterns=[
 url(r'^hello-view/',views.HelloApiView.as_view()),
+url(r'',include(router.urls))
 ]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .import serializers
+from rest_framework import status
 
 # Create your views here.
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
-
+from . import models
+from .import permissions
+from rest_framework.authentication import TokenAuthentication
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class=serializers.HelloSerializer
     def get(self, request, format=None):
         """Returns a list of api view features"""
         an_apiview=[
@@ -14,3 +20,18 @@ class HelloApiView(APIView):
         'manually mapped',
         ]
         return Response({'message':'Hello','an_apiview':an_apiview})
+    def post(self,request):
+        serializer=serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name=serializer.data.get('name')
+            message='Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,
+             status=status.HTTP_400_BAD_REQUEST)
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
