4661430c58f8815315e3c112c1ee0db156ae9602
prahladchaudhary14 <53113794+prahladchaudhary14@users.noreply.github.com>
2020-08-19 23:15:25 +0530

    aded

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -10,7 +10,6 @@ from . import models
 from .import permissions
 from rest_framework.authentication import TokenAuthentication
 import random
-import time
 import datetime
 
 """Note: Use 24 Hour Clock for the code to work"""
@@ -30,71 +29,12 @@ Time Remaining -> Time Remaining
 """
 
 
-
-# Returns starting minute of the block
-def function(time_m: str, blk: str = 'n') -> str:
-    if blk == 'p':
-        block_region = int(time_m) // 15 + 1
-        op = (block_region - 1) * 15
-        return str(op) * 2 if 1 == len(str(op)) else str(op)
-    else:
-        op = str(((int(time_m) // 15 + 1) * 15) % 60)
-        return op * 2 if 1 == len(op) else op
-
-
-def update_values_15_min():
-    global dc, sg, ag, frequency, current_block, next_block, current_block_number, next_block_start, current_block_end
-
-    dc = round(150 + random.random() * 50, 2)
-    sg = round(150 + random.random() * 50, 2)
-    ag = round(150 + random.random() * 50, 2)
-
-    frequency = round(49.5 + random.random() * 1.5, 2)
-
-    if current_block_number == 96:
-        current_block_number = 1
-        update_values_1_day()
-    else:
-        current_block_number += 1
-
-    next_block_start = current_block_end
-    current_block = next_block
-    next_block = get_next_from_current(current_block)
-    current_block_end = current_block[-5:]
-
-
-# Update value every day
-def update_values_1_day():
-    global fuel_price
-    fuel_price = round(2 + random.random() * 8, 2)
-
-
 # Takes digit in int form and converts it to two digits (if only one) by adding preceding zero, returns str
 def digit_convert(number: int) -> str:
     if len(str(number)) == 1:
         return '0' + str(number)
     else:
         return str(number)
-# Get next block from the current block
-def get_next_from_current(current):
-    nb_start_hh, nb_start_mm = current[-5:-3], current[-2:]
-    nb_end_hh = digit_convert((int(nb_start_hh) + 1) % 24) if nb_start_mm == '45' else digit_convert(int(nb_start_hh))
-    nb_end_mm = digit_convert((int(nb_start_mm) + 15) % 60)
-
-    return nb_start_hh + ":" + nb_start_mm + '-' + nb_end_hh + ":" + nb_end_mm
-
-
-# Function to update elapsed and remaining time; takes str(datetime-now) as input
-def time_elapsed_remaining(temps):
-    global time_elapsed, time_remaining, time_elapsed_mm, time_elapsed_ss, time_remaining_mm, time_remaining_ss, time_remaining_in_seconds
-
-    time_elapsed_mm = digit_convert(int(temps[3:5]) % 15)
-    time_elapsed_ss = digit_convert(int(temps[6:8]))
-
-    time_remaining_in_seconds = digit_convert(15 * 60 - (int(time_elapsed_mm) * 60 + int(time_elapsed_ss)))
-    time_remaining_mm, time_remaining_ss = digit_convert(int(time_remaining_in_seconds) // 60), digit_convert(int(time_remaining_in_seconds) % 60)
-
-    time_elapsed, time_remaining = time_elapsed_mm + ':' + time_elapsed_ss, time_remaining_mm + ':' + time remaining_ss
 
 
 t = str(datetime.datetime.now().time())
@@ -142,8 +82,8 @@ ag = round(150 + random.random() * 50, 2)
 
 frequency = round(49.5 + random.random() * 1.5, 2)
 
-# Update each day (24-hours)
-fuel_price = round(2 + random.random() * 8, 2)
+# Updated every 24 hours
+fuel_price = round(2 + random.random() * 8, 2);
 null=0;
 class PowerPlantData(APIView):
     """Test API View"""
