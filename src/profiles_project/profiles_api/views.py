c245b3027bbc52785d863fde88c28b0f552174b1
prahladchaudhary14 <53113794+prahladchaudhary14@users.noreply.github.com>
2020-08-25 23:12:16 +0530

    new added

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -32,6 +32,77 @@ null=0;
 class PowerPlantData(APIView):
     """Test API View"""
     def get(self, request, format=None):
+
+
+        """Read time from the system clock, using that, Assign current block number, current block and next block """
+        t = str(datetime.datetime.now().time())
+        t_hh, t_mm, t_ss = t[:2], t[3:5], t[6:8]
+        current_block_place = int(t_mm) // 15 + 1
+        next_block_place = 1 if current_block_place == 4 else current_block_place + 1
+
+        current_block_number = (int(t_hh) * 60 + int(t_mm)) // 15 + 1
+        current_block_start = t_hh + ":" + digit_convert((int(t_mm) // 15) * 15)
+        next_block_start = digit_convert(((int(t_hh) + 1) % 24) if current_block_place == 4 else t_hh) + ":" + function(t_mm)
+        current_block_end = next_block_start
+        next_block_end = digit_convert((int(next_block_start[:2]) + 1) % 24) + ":" + "00" if next_block_place == 4 else digit_convert(int(next_block_start[:2])) + ":" + digit_convert(int(next_block_start[-2:]) + 15)
+
+        current_block = current_block_start + "-" + current_block_end
+        next_block = next_block_start + "-" + next_block_end
+
+        """From the time (used above) find the time elapsed and the time remaining"""
+        time_elapsed_mm, time_elapsed_ss = digit_convert(int(t_mm) % 15), digit_convert(int(t_ss))
+        time_remaining_in_seconds = digit_convert(15 * 60 - (int(time_elapsed_mm) * 60 + int(time_elapsed_ss)))
+        time_remaining_mm, time_remaining_ss = digit_convert(int(time_remaining_in_seconds) // 60), digit_convert(int(time_remaining_in_seconds) % 60)
+
+        time_elapsed = time_elapsed_mm + ":" + time_elapsed_ss
+        time_remaining = time_remaining_mm + ":" + time_remaining_ss
+
+
+        """Randomizing the values as per the specified ranges"""
+
+        random.seed(current_block_number)
+        # Update in intervals of 15 minutes (1 time block)
+        dc = round(150 + random.random() * 50, 2)
+        sg = round(150 + random.random() * 50, 2)
+        ag = round(150 + random.random() * 50, 2)
+        frequency = round(49.5 + random.random() * 1.5, 2)
+        deviation = ag - sg
+
+        rupees = rupees_calculate()
+
+        # Charges
+        ui_dev_charge = ui_dev_charge_calculate()
+        ui_dev_charge_above_and_150 = ui_dev_charge_above_and_150_calculate()
+        ui_dev_charge_above_and_012 = ui_dev_charge_above_and_012_calculate()
+        ui_dev_charge_below_dc = ui_dev_charge_below_dc_calculate()
+        ui_dev_charge_below_dc_add = ui_dev_charge_below_dc_add_calculate()
+        oi_dev_charge = oi_dev_charge_calculate()
+        oi_dev_charge_add = oi_dev_charge_add_calculate()
+
+        sum_ui = ui_dev_charge + ui_dev_charge_above_and_150 + ui_dev_charge_above_and_012 + ui_dev_charge_below_dc + ui_dev_charge_below_dc_add
+        total_charge = sum_ui + oi_dev_charge + oi_dev_charge_add
+        total_charge_add = ui_dev_charge_above_and_150 + ui_dev_charge_above_and_012 + ui_dev_charge_below_dc_add + oi_dev_charge_add
+
+        fuel = deviation * 250 * 151.4 / 100 * (-1)
+        net_gain = fuel + total_charge
+
+
+        # SG for the next four blocks
+        random.seed(block_number(current_block_number + 1))
+        sg_plus_one = round(150 + random.random() * 50, 2)
+
+        random.seed(block_number(current_block_number + 2))
+        sg_plus_two = round(150 + random.random() * 50, 2)
+
+        random.seed(block_number(current_block_number + 3))
+        sg_plus_three = round(150 + random.random() * 50, 2)
+
+        random.seed(block_number(current_block_number + 4))
+        sg_plus_four = round(150 + random.random() * 50, 2)
+
+        # Updated every 24 hours
+        random.seed(int((str(datetime.date.today())[-2:])))
+        fuel_price = round(2 + random.random() * 8, 2)
         """Returns a list of api view features"""
 
         """
@@ -90,7 +161,6 @@ class PowerPlantData(APIView):
 
         def rupees_calculate():
             global frequency
-
             if 49.48 <= frequency <= 50.00:
                 return 3.0304
             elif frequency == 50.01:
@@ -184,76 +254,6 @@ class PowerPlantData(APIView):
             else:
                 return 0
 
-
-        """Read time from the system clock, using that, Assign current block number, current block and next block """
-        t = str(datetime.datetime.now().time())
-        t_hh, t_mm, t_ss = t[:2], t[3:5], t[6:8]
-        current_block_place = int(t_mm) // 15 + 1
-        next_block_place = 1 if current_block_place == 4 else current_block_place + 1
-
-        current_block_number = (int(t_hh) * 60 + int(t_mm)) // 15 + 1
-        current_block_start = t_hh + ":" + digit_convert((int(t_mm) // 15) * 15)
-        next_block_start = digit_convert(((int(t_hh) + 1) % 24) if current_block_place == 4 else t_hh) + ":" + function(t_mm)
-        current_block_end = next_block_start
-        next_block_end = digit_convert((int(next_block_start[:2]) + 1) % 24) + ":" + "00" if next_block_place == 4 else digit_convert(int(next_block_start[:2])) + ":" + digit_convert(int(next_block_start[-2:]) + 15)
-
-        current_block = current_block_start + "-" + current_block_end
-        next_block = next_block_start + "-" + next_block_end
-
-        """From the time (used above) find the time elapsed and the time remaining"""
-        time_elapsed_mm, time_elapsed_ss = digit_convert(int(t_mm) % 15), digit_convert(int(t_ss))
-        time_remaining_in_seconds = digit_convert(15 * 60 - (int(time_elapsed_mm) * 60 + int(time_elapsed_ss)))
-        time_remaining_mm, time_remaining_ss = digit_convert(int(time_remaining_in_seconds) // 60), digit_convert(int(time_remaining_in_seconds) % 60)
-
-        time_elapsed = time_elapsed_mm + ":" + time_elapsed_ss
-        time_remaining = time_remaining_mm + ":" + time_remaining_ss
-
-
-        """Randomizing the values as per the specified ranges"""
-
-        random.seed(current_block_number)
-        # Update in intervals of 15 minutes (1 time block)
-        dc = round(150 + random.random() * 50, 2)
-        sg = round(150 + random.random() * 50, 2)
-        ag = round(150 + random.random() * 50, 2)
-        frequency = round(49.5 + random.random() * 1.5, 2)
-        deviation = ag - sg
-
-        rupees = rupees_calculate()
-
-        # Charges
-        ui_dev_charge = ui_dev_charge_calculate()
-        ui_dev_charge_above_and_150 = ui_dev_charge_above_and_150_calculate()
-        ui_dev_charge_above_and_012 = ui_dev_charge_above_and_012_calculate()
-        ui_dev_charge_below_dc = ui_dev_charge_below_dc_calculate()
-        ui_dev_charge_below_dc_add = ui_dev_charge_below_dc_add_calculate()
-        oi_dev_charge = oi_dev_charge_calculate()
-        oi_dev_charge_add = oi_dev_charge_add_calculate()
-
-        sum_ui = ui_dev_charge + ui_dev_charge_above_and_150 + ui_dev_charge_above_and_012 + ui_dev_charge_below_dc + ui_dev_charge_below_dc_add
-        total_charge = sum_ui + oi_dev_charge + oi_dev_charge_add
-        total_charge_add = ui_dev_charge_above_and_150 + ui_dev_charge_above_and_012 + ui_dev_charge_below_dc_add + oi_dev_charge_add
-
-        fuel = deviation * 250 * 151.4 / 100 * (-1)
-        net_gain = fuel + total_charge
-
-
-        # SG for the next four blocks
-        random.seed(block_number(current_block_number + 1))
-        sg_plus_one = round(150 + random.random() * 50, 2)
-
-        random.seed(block_number(current_block_number + 2))
-        sg_plus_two = round(150 + random.random() * 50, 2)
-
-        random.seed(block_number(current_block_number + 3))
-        sg_plus_three = round(150 + random.random() * 50, 2)
-
-        random.seed(block_number(current_block_number + 4))
-        sg_plus_four = round(150 + random.random() * 50, 2)
-
-        # Updated every 24 hours
-        random.seed(int((str(datetime.date.today())[-2:])))
-        fuel_price = round(2 + random.random() * 8, 2)
         return Response({
            "Dc":dc,
            "Sg":sg,
